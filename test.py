ead5e56e8797d7948d80e13491198c16720d69f4
renan_m_amorim <renan_m_amorim@hotmail.com>
2019-01-19 16:09:55 -0500

    update files for CI deploy

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "4Linux - Devops Essentials by Renan Amorim.")
+        self.assertRegex(result.data.decode(), "4Linux - Devops Essentials")
 
 
 if __name__ == "__main__":
